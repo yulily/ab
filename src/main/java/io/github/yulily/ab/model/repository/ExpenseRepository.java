e0d10fd696ee380737d1032840b671a915456444
shiri <irihsmoc@gmail.com>
2024-10-14 22:23:38 +0900

    add: メンバートップ追加

--- src/main/java/io/github/yulily/ab/model/repository/ExpenseRepository.java
+++ src/main/java/io/github/yulily/ab/model/repository/ExpenseRepository.java
@@ -13,9 +13,12 @@ public interface ExpenseRepository extends CrudRepository<Expense, Long> {
     @Query("SELECT SUM(e.amount) FROM Expense e WHERE e.member.user.id = ?1 AND e.paymentAt BETWEEN ?2 AND ?3")
     Integer sumExpensesByUserId(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT e.member.memberName, SUM(e.amount) FROM Expense e WHERE e.member.id IN ?1 AND e.paymentAt BETWEEN ?2 AND ?3 GROUP BY e.member.id, e.member.memberName")
+    @Query("SELECT e.member.id, e.member.memberName, SUM(e.amount) FROM Expense e WHERE e.member.id IN ?1 AND e.paymentAt BETWEEN ?2 AND ?3 GROUP BY e.member.id, e.member.memberName")
     List<Object[]> sumExpensesByMemberIds(List<Long> memberIds, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT c.categoryName, SUM(e.amount) FROM Expense e JOIN e.category c WHERE e.member.user.id = ?1 AND e.paymentAt BETWEEN ?2 AND ?3 GROUP BY e.category.id, c.categoryName")
+    @Query("SELECT c.id, c.categoryName, SUM(e.amount) FROM Expense e JOIN e.category c WHERE e.member.user.id = ?1 AND e.paymentAt BETWEEN ?2 AND ?3 GROUP BY e.category.id, c.categoryName")
     List<Object[]> sumExpensesByUserIdGroupByCategory(Long userId, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT DATE(e.paymentAt), SUM(e.amount) FROM Expense e WHERE e.member.id = :memberId AND e.paymentAt BETWEEN :startOfMonth AND :endOfMonth GROUP BY DATE(e.paymentAt) ORDER BY DATE(e.paymentAt)")
+    List<Object[]> sumExpensesByMemberGroupByDay(Long memberId, LocalDateTime startOfMonth, LocalDateTime endOfMonth);
 }
